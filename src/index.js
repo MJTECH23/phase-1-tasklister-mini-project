d82d9f26a6a732218210e302c6228341ebd84389
MJTECH23 <mich.jaoko@gmail.com>
2023-03-25 22:46:22 +0300

    commit message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskWrapper = document.querySelector("#tasks");
+  const inputTask = document.querySelector("#new-task-description");
+
+  // creating the button
+  const submitButton = document.querySelector("input[type=\"submit]")
+  const form = document.querySelector('#create-task-form')
+
+  const validInput = (input) => {
+    input = input.trim();
+    return input ? input : false;
+  };
+// adding a task
+  const addNewTask = (e) => {
+    e.preventDefault();
+    getNewTask(); // calling a valid task
+  };
+
+  const getNewTask = () => {
+    let task = inputTask.value;
+    if (validInput(task)) {
+      buildNewTask(task);
+    }
+  };
+
+  const buildNewTask = (taskContent) => {
+    let task = document.createElement("li");
+    task.textContent = taskContent;
+    taskWrapper.appendChild(task);
+    form.reset();
+  };
+
+  form.addEventListener("submit", addNewTask);
+
+  
